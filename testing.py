b0e9a7318d5019eee75f7be9ec40633fb2ae472c
Robin <robin.thibaut@ugent.be>
2020-05-06 09:00:20 +0200

    Minor changes

--- testing.py
+++ testing.py
@@ -2,13 +2,25 @@ import os
 from os.path import join as join_path
 import toolbox
 
+# Define working directory
 cwd = os.getcwd()
+# Define datasets directory
 data_dir = join_path(cwd, 'datasets', 'benin')
+# Define file name
 f_name = 'Dataset_Log_WithoutOutlier_WithoutDouble(LowerThan30m)_Without-4.txt'
+# Initialize problem
 sgems = toolbox.Sgems(data_dir=data_dir, file_name=f_name, dx=1000, dy=1000)
+# Display point coordinates and grid
 sgems.plot_coordinates()
+# Compute data points node and export file
 sgems.export_node_idx()
+# Load your algorithm xml file in the 'algorithms' folder
 algo_name = sgems.xml_reader('cokriging')
+# Show xml structure tree
 sgems.show_tree()
+# Modify xml below:
+
+# Write python script
 sgems.write_command()
+# Run sgems
 sgems.run()
