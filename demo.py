632ae144807e6762b44258a125ebbd491bef27c3
Robin <robin.thibaut@ugent.be>
2020-05-08 11:13:47 +0200

    Minor changes

--- demo.py
+++ demo.py
@@ -6,17 +6,17 @@ import toolbox
 # Define working directory
 cwd = os.getcwd()
 # Define datasets directory
-data_dir = join_path(cwd, 'datasets', 'benin')
+data_dir = join_path(cwd, 'datasets', 'demo')
 # Define file name
-f_name = 'Dataset_Log_WithoutOutlier_WithoutDouble(LowerThan30m)_Without-4.txt'
+f_name = 'sgems_dataset.txt'
 # Initialize problem
-sgems = toolbox.Sgems(dx=5000, dy=5000, data_dir=data_dir, file_name=f_name)
+sgems = toolbox.Sgems(dx=2, dy=2, data_dir=data_dir, file_name=f_name)
 # Generate grid
 sgems.generate_grid()
 # Display point coordinates and grid
 sgems.plot_coordinates()
 # Load your algorithm xml file in the 'algorithms' folder
-algo_name = sgems.xml_reader('cokriging')
+algo_name = sgems.xml_reader('kriging')
 # Show xml structure tree
 sgems.show_tree()
 # Modify xml below:
