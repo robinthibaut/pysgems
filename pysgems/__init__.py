03d13a45bff1cd2e25695bbde3bbb1f3ae165374
Robin <robin.thibaut@ugent.be>
2020-05-13 16:16:57 +0200

    minor changes

---
+++ pysgems/__init__.py
@@ -0,0 +1,11 @@
+__name__ = 'pysgems'
+__author__ = 'Robin Thibaut'
+
+from . import algo
+from . import base
+from . import dis
+from . import examples
+from . import io
+from . import plot
+from . import sgems
+from . import utils
