3dcd63c388d5f498854c50442b5df2ff9d473ce2
Robin <robin.thibaut@ugent.be>
2020-12-11 16:43:35 +0100

    Added setup for pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+my_pckg = find_packages(include=['pysgems'], exclude=['pysgems.deprecated', 'pysgems.develop'])
+print(my_pckg)
+
+setup(
+    name='pysgems',
+    version='1.1.6',
+    packages=my_pckg,
+    include_package_data=True,
+    url='https://github.com/robinthibaut/pysgems',
+    license='MIT',
+    author='Robin Thibaut',
+    author_email='robin.thibaut@UGent.be',
+    description='Use SGeMS (Stanford Geostatistical Modeling Software) within Python.',
+    install_requires=['numpy', 'pandas', 'scipy', 'matplotlib', 'shapely'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
