cf5dcd9ea6573df96b99e6332b6e290c6d1cfc5e
Robin Thibaut <robin.thibaut@ugent.be>
2021-06-05 12:43:05 +0200

    Documentation

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,16 @@ author = 'Robin Thibaut'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc"
-]
+extensions = ["sphinx.ext.autodoc",
+              "sphinx_rtd_theme",
+              ]
 
+autodoc_default_options = {'members': True,
+                           'undoc-members': True,
+                           'private-members': True,
+                           'special-members': True,
+                           'inherited-members': True,
+                           'show-inheritance': True}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -44,7 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
