d4be15bab2ebe0ad172d6af4d7340b3c153c9201
Robin Thibaut <robin.thibaut@ugent.be>
2021-11-25 15:39:13 +0100

    Docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,31 +12,52 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
 
+# sys.path.insert(2, os.path.abspath('../skbel'))
+# sys.path.insert(1, os.path.abspath('../examples'))
+from datetime import datetime
+
+sys.path.insert(0, os.path.abspath('../'))
 
 # -- Project information -----------------------------------------------------
 
 project = 'pysgems'
-copyright = '2021, Robin Thibaut'
+copyright = f"2019 - {datetime.now().year} Robin Thibaut"
 author = 'Robin Thibaut'
-
 
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.autodoc",
-              "sphinx_rtd_theme",
-              ]
+# extensions = ["sphinx.ext.autodoc",
+#               "sphinx_rtd_theme",
+#               ]
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.doctest",
+    "sphinx_rtd_theme",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.extlinks",
+    "nbsphinx",
+    "matplotlib.sphinxext.plot_directive",
+    "IPython.sphinxext.ipython_directive",
+    "IPython.sphinxext.ipython_console_highlighting",
+    "sphinx.ext.inheritance_diagram",
+]
+
 
 autodoc_default_options = {'members': True,
                            'undoc-members': True,
                            'private-members': True,
-                           'special-members': True,
-                           'inherited-members': True,
-                           'show-inheritance': True}
+                           'special-members': '__init__, __call__',
+                           'inherited-members': False,
+                           'show-inheritance': False}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -45,13 +66,21 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = ['.rst', '.md']
+
+add_function_parentheses = False
+pygments_style = "sphinx"
+html_domain_indices = False
+html_use_index = False
+html_copy_source = True
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
+# The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
 html_theme = "sphinx_rtd_theme"
+# html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
